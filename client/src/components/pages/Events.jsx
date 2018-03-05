78427880fc4502a0965ae4935d5f7be305ea5386
Deniz Skantz <denizskantz@hotmail.com>
2018-03-05 16:19:42 -0500

    halenapp

--- client/src/components/pages/Events.jsx
+++ client/src/components/pages/Events.jsx
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
 // import MoreInfo from "./MoreInfo";
 import SingleEvent from "./SingleEvent";
+import { render } from 'react-dom';
+import Geolocation from "react-geolocation";
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import { compose, withProps, withStateHandlers } from "recompose"
+import ReactGoogleMapLoader from "react-google-maps-loader";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from "react-google-maps";
+
+const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
+const Maps = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div className="map" style={{ height: `700px`, width: `500px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap,
+)((props) =>
+  <GoogleMap
+    defaultZoom={13}
+    defaultCenter={props.position || { lat: 40.7831, lng: -73.9712 }}
+  >
+      <MarkerWithLabel
+      position={props.position || { lat: 40.7831, lng: -73.9712 }}
+      labelAnchor={new window.google.maps.Point(0, 0)}
+      labelStyle={{background: "white", fontSize: "15px", padding: "10px"}}
+    >
+      <div>You are Here!</div>
+    </MarkerWithLabel>
+  </GoogleMap>
+);
 
 
 class Events extends Component {
@@ -8,14 +46,16 @@ class Events extends Component {
     super(props)
     this.state = {
       directions: [],
-      showMore: false
+      showMore: false,
+      isMarkerShown: false,
     };
-
+    this.delayedShowMarker = this.delayedShowMarker.bind(this);
     this.handleLocationCall = this.handleLocationCall.bind(this);
     this.handleShowMore = this.handleShowMore.bind(this);
   }
 
   componentDidMount() {
+    this.delayedShowMarker();
     this.handleLocationCall();
   }
 
@@ -26,9 +66,45 @@ class Events extends Component {
   }
 
 
+  delayedShowMarker() {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true,
+           })
+    }, 3000)
+  }
+
   render() {
     return (
     <div className="event-list">
+    <section id="explore" className="clearfix">
+      <article>
+      <Geolocation
+      onSuccess={ (position) => {this.setState({position: {lat: position.coords.latitude, lng: position.coords.longitude}}); console.log(position)}}
+      render={({
+        fetchingPosition,
+        position: { coords: { latitude, longitude } = {} } = {},
+        error,
+        getCurrentPosition
+      }) =>
+        <div>
+          <button onClick={getCurrentPosition}>Get Position</button>
+          {error &&
+            <div>
+              {error.message}
+            </div>}
+        </div>}
+    />
+      <div id="relative">
+      <Maps
+        isMarkerShown={this.state.isMarkerShown}
+        // onMarkerClick={this.handleMarkerClick}
+        position={this.state.position}
+
+      />
+      </div>
+      </article>
+
+
     <h1 className="button medium"> Here are Today's Events </h1>
       {this.props.apiData.map((event, index)=> (
         <ul>
@@ -37,6 +113,7 @@ class Events extends Component {
       </li>
       </ul>
           ))}
+      </section>
     </div>
     )
   }
--- client/src/components/pages/PastEvents.jsx
+++ client/src/components/pages/PastEvents.jsx
@@ -24,32 +24,25 @@ class PastEvents extends Component {
     }
 
     render() {
-        console.log("events inside pastevents", this.state.eventsData)
-        // let eventContainer =[];
-        // for (const event in this.state.eventsData){
-        //   console.log('event container', event);
-        //  eventContainer.push(<div><span> <strong>Name: </strong>{event.displayName}</span><span> <strong>Type:</strong> {event.type}</span><span> <strong>Venue:</strong> {event.venue}</span><span>  <strong>Date:</strong> {event.dateEvent}</span><span> <strong>URL:</strong> {event.url}</span></div>)
-
-        // }
           return (
             <section className="past">
-                <h1 className="pastmedium">Past Events List</h1>
-                <div>
+              <h1 className="pastmedium">Past Events List</h1>
+              <div>
                 {this.state.eventsDataLoaded ? (
-            <div>
+                <div className="pastmedium">
                   {this.state.eventsData.map(event => (
-              <div key={event.id}>
-              <h1>  Name: {event.displayname} </h1>
-              <p>  Type: {event.type} </p>
-              <p>  Venue: {event.venue} </p>
-              <p>  Date: {event.dateevent} </p>
-              <p>  URL: <a className= "singleEvent" href={event.url}> See Event Page </a> </p>
-            </div>
-                ))}
-            </div>) : (
-              <p> Loading Events... </p>
-          )}
-                </div>
+                  <div key={event.id}>
+                    <h1> Name: {event.displayname} </h1>
+                    <p>  Type: {event.type} </p>
+                    <p>  Venue: {event.venue} </p>
+                    <p>  Date: {event.dateevent} </p>
+                    <p>  URL: <a className= "singleEvent" href={event.url}> See Event Page </a> </p>
+                  </div>
+                      ))}
+                </div>) : (
+                  <p> Loading Events... </p>
+                )}
+              </div>
 
             </section>
           );
