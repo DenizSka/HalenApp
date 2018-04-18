ed498993ccceba092f0ec649b50a6b247d5d4152
Deniz Skantz <denizskantz@hotmail.com>
2018-04-18 16:38:35 -0400

    appjs css  dist instead of public

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
+
 
 app.use('/pastevents', pastEvents);
 app.use('/events', events);
