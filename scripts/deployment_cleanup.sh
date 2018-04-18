62f2c442948309bd5d11b60ce3b8d7f19d407808
Deniz Skantz <denizskantz@hotmail.com>
2018-04-18 12:55:22 -0400

    package json revised

---
+++ scripts/deployment_cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# @desc Deployment should be as slim as possible, and with any identifying remarks or traces removed (i.e., we don't need to tell anyone how we built our app when it's deployed ;).
+
+rm -rf db src scripts starter_resources lessons webpack.config.js .babelrc .gitignore README.md
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello, world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root"));
+registerServiceWorker();
+
